9d694f4f45a51e641886a19e10aa16e0839afadf
“Georgiy <varlamovg@mail.ru>
2019-01-04 01:44:03 +0300

    Создана форма логина, добавлен соответствующий шаблон в templates/login.html

---
+++ webapp/webapp/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+from webapp.forms import LoginForm
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    #db.init_app(app)
+
+    @app.route('/')
+    def index():
+        return 'OK'
+
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    
+    return app
+
+if __name__ == '__main__' :
+    app.run(host='0.0.0.0')
+
