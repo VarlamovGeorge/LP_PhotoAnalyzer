011d07e71b8179f80a3c8440536e314965b8af9b
Dmitry Abramov <a6pamob@yandex.ru>
2019-01-10 18:04:03 +0300

    Добавляем известные типы хранилищ

--- webapp/create_db.py
+++ webapp/create_db.py
@@ -1,3 +1,15 @@
 from webapp import db, create_app
+from webapp.model import Storages
 
-db.create_all(app=create_app())
+app = create_app()
+db.create_all(app=app)
+
+with app.app_context():
+    dropbox = Storages(id=1, name='dropbox', full_path='')
+    yadisk = Storages(id=2, name='yadisk', full_path='')
+
+    db.session.add(dropbox)
+    db.session.add(yadisk)
+
+    db.session.commit()
+   
