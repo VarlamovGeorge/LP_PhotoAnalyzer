8d04177789e1d14f50fe8bf5f4238cfe70df741f
“Georgiy <varlamovg@mail.ru>
2019-01-18 20:17:21 +0300

    Тип поля algorithms.create_date изменен с Date на DateTime.

--- cnn/trained/cnn_properties.py
+++ cnn/trained/cnn_properties.py
@@ -1,4 +1,4 @@
-create_date = '2019-01-12'
+create_date = '2019-01-12 01:01:01'
 description = 'ResNet50, 9 classes, 0.89 accuracy'
 classes_list = [
     'cats', 'dogs', 'cars', 'humans', 'landscapes', 'food', 'cities', 'documents', 'other'
--- webapp/create_db.py
+++ webapp/create_db.py
@@ -1,15 +1,33 @@
 from webapp import db, create_app
-from webapp.model import Storages
+from webapp.model import Storages, Classes
+
+classes_dict = [
+    {'id': 1, 'label': 'cats'},
+    {'id': 2, 'label': 'dogs'},
+    {'id': 3, 'label': 'cars'},
+    {'id': 4, 'label': 'humans'},
+    {'id': 5, 'label': 'landscapes'},
+    {'id': 6, 'label': 'food'},
+    {'id': 7, 'label': 'cities'},
+    {'id': 8, 'label': 'documents'},
+    {'id': 9, 'label': 'other'}
+    ]
 
 app = create_app()
 db.create_all(app=app)
 
 with app.app_context():
+    # Создаем записи в таблице storages
     dropbox = Storages(id=1, name='dropbox', full_path='')
     yadisk = Storages(id=2, name='yadisk', full_path='')
 
     db.session.add(dropbox)
     db.session.add(yadisk)
-
+    
+    # Создаем словарь базовых классов
+    for item in classes_dict:
+        class_ = Classes(id=(classes_dict.index(item)+1), name=item['label'])
+        db.session.add(class_)
+    
     db.session.commit()
    
