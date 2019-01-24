f2940cbdfb9d281e0ddaa5744f1b4ac14031b16f
“Georgiy <varlamovg@mail.ru>
2019-01-25 01:04:53 +0300

    Добавлен AJAX для отображения результатов (пока не работает distinct в выводе)

--- webapp/webapp/__init__.py
+++ webapp/webapp/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, flash, redirect, request, url_for
+from flask import Flask, render_template, flash, redirect, request, url_for, jsonify
 from flask_login import current_user, LoginManager, login_user, logout_user, login_required
 from flask_migrate import Migrate
+from datetime import datetime
 
 from webapp.model import * #db, Users
 from webapp.settings.models import StorageUsers, Storages, UserPreferences
@@ -62,26 +63,80 @@ def create_app():
         flash('Вы успешно разлогинились')
         return redirect(url_for('index'))
 
-    @app.route('/search', methods=['POST'])
+    @app.route('/search')
     def search():
-        selected_classes = request.form.getlist('class')
-        selected_start_date = request.form.get('start_date')
-        selected_end_date = request.form.get('end_date')
+        class_list = []
+        cats = request.args.get('cats', 'false')
+        if cats != 'false':
+            class_list.append('cats')
+        dogs = request.args.get('dogs', 'false')
+        if dogs != 'false':
+            class_list.append('dogs')
+        humans = request.args.get('humans', 'false')
+        if humans != 'false':
+            class_list.append('humans')
+        cars = request.args.get('cars', 'false')
+        if cars != 'false':
+            class_list.append('cars')
+        cities = request.args.get('cities', 'false')
+        if cities != 'false':
+            class_list.append('cities')
+        landscapes = request.args.get('landscapes', 'false')
+        if landscapes != 'false':
+            class_list.append('landscapes')
+        food = request.args.get('food', 'false')
+        if food != 'false':
+            class_list.append('food')
+        documents = request.args.get('documents', 'false')
+        if documents != 'false':
+            class_list.append('documents')
+        other = request.args.get('other', 'false')
+        if other != 'false':
+            class_list.append('other')
+        
+        print(class_list)
+
+        start_date = request.args.get('start_date', '1970-01-01')
+        end_date = request.args.get('end_date', '3000-01-01')
+        
+        if start_date!='':   
+            start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        if end_date!='':
+            end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        
         current_user_pref = UserPreferences.query.filter(UserPreferences.user_id==current_user.id).first()
         threshold = current_user_pref.classification_threshold
-        print(selected_classes, selected_start_date, selected_end_date, threshold)
-        print(type(selected_classes), type(selected_start_date), type(selected_end_date), type(threshold))
+        print(start_date, end_date, threshold)
+        print(type(start_date), type(end_date), type(threshold))
 
         selected_photos = Photos.query. \
             join(photosclasses). \
             filter(photosclasses.c.weight>threshold). \
             join(Classes). \
-            filter(Classes.name.in_(selected_classes)).all()
-        
-        for ph in selected_photos:
-            print(ph.id)
+            filter(Classes.name.in_(class_list)).distinct().all()
 
-        return redirect(url_for('index'))
+        # selected_photos = db.session.query(Photos.id, max(Classes.name)). \
+        #     join(photosclasses, Classes). \
+        #     filter(photosclasses.c.weight>threshold, Classes.name.in_(class_list)). \
+        #     all()
+        
+        #selected_photos = selected_photos(Photos.id, Photos.name).group_by(Photos.id).all()
+        #highest_order.c.customer_id
+        # ph_str = ''
+        # for ph in selected_photos:
+        #     ph_str += '<li class=\"list-group-item\">'+str(ph.id)+'</li>\n'
+            
+        # print(ph_str)
+        ph_str = ''
+        ph_list = []
+        for ph in selected_photos:
+            ph_str += '<li class=\"list-group-item\">'+str(ph.id)+' '+str(ph.name)+'</li>\n'
+            ph_list.append(ph_str)
+
+        print(ph_list)
+
+        #return jsonify(result=cats+dogs+humans+cars+cities+landscapes+food+documents+other+str(start_date)+str(end_date))
+        return jsonify(result=ph_list)
 
     return app
 
---
+++ webapp/webapp/static/ph.js
@@ -0,0 +1,22 @@
+$(function() {
+$('button#calculate').bind('click', function() {
+  $.getJSON($SCRIPT_ROOT + '/search', {
+    cats: $('input[id="defaultCheck1"]').prop('checked'),
+    dogs: $('input[id="defaultCheck2"]').prop('checked'),
+    humans: $('input[id="defaultCheck3"]').prop('checked'),
+    cars: $('input[id="defaultCheck4"]').prop('checked'),
+    cities: $('input[id="defaultCheck5"]').prop('checked'),
+    landscapes: $('input[id="defaultCheck6"]').prop('checked'),
+    food: $('input[id="defaultCheck7"]').prop('checked'),
+    documents: $('input[id="defaultCheck8"]').prop('checked'),
+    other: $('input[id="defaultCheck9"]').prop('checked'),
+    start_date: $('input[id="start_date"]').val(),
+    end_date: $('input[id="end_date"]').val()
+  }, function(data) {
+    $("#result").text(data.result);
+    $('#scroll-group').html(data.result);
+    $("#scroll-group").show();
+  });
+  return false;
+});
+});
