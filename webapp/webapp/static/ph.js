e2e260efe57c96d3ff0e3d16e285fb44f14fdc5f
“Georgiy <varlamovg@mail.ru>
2019-01-25 23:20:20 +0300

    Добавлен обработчик exception получения данных из БД

--- webapp/webapp/static/ph.js
+++ webapp/webapp/static/ph.js
@@ -1,22 +1,26 @@
 $(function() {
-$('button#calculate').bind('click', function() {
-  $.getJSON($SCRIPT_ROOT + '/search', {
-    cats: $('input[id="defaultCheck1"]').prop('checked'),
-    dogs: $('input[id="defaultCheck2"]').prop('checked'),
-    humans: $('input[id="defaultCheck3"]').prop('checked'),
-    cars: $('input[id="defaultCheck4"]').prop('checked'),
-    cities: $('input[id="defaultCheck5"]').prop('checked'),
-    landscapes: $('input[id="defaultCheck6"]').prop('checked'),
-    food: $('input[id="defaultCheck7"]').prop('checked'),
-    documents: $('input[id="defaultCheck8"]').prop('checked'),
-    other: $('input[id="defaultCheck9"]').prop('checked'),
-    start_date: $('input[id="start_date"]').val(),
-    end_date: $('input[id="end_date"]').val()
-  }, function(data) {
-    $("#result").text(data.result);
-    $('#scroll-group').html(data.result);
-    $("#scroll-group").show();
+  $('button#calculate').bind('click', function() {
+    $.getJSON($SCRIPT_ROOT + '/search', {
+      cats: $('input[id="defaultCheck1"]').prop('checked'),
+      dogs: $('input[id="defaultCheck2"]').prop('checked'),
+      humans: $('input[id="defaultCheck3"]').prop('checked'),
+      cars: $('input[id="defaultCheck4"]').prop('checked'),
+      cities: $('input[id="defaultCheck5"]').prop('checked'),
+      landscapes: $('input[id="defaultCheck6"]').prop('checked'),
+      food: $('input[id="defaultCheck7"]').prop('checked'),
+      documents: $('input[id="defaultCheck8"]').prop('checked'),
+      other: $('input[id="defaultCheck9"]').prop('checked'),
+      start_date: $('input[id="start_date"]').val(),
+      end_date: $('input[id="end_date"]').val()
+    }, function(data) {
+      if ( String(data.result)=="error" ){
+        $("#result").text("Ошибка получения данных из БД");
+      } else {
+        $("#result").text(data.result);
+        $('#scroll-group').html(data.result);
+        $("#scroll-group").show();
+      };
+    });
+    return false;
   });
-  return false;
-});
 });
