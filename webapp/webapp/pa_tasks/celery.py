02ba63c1a9a65ba21df2a028491c47da28911750
Dmitry Abramov <a6pamob@yandex.ru>
2019-01-28 01:27:53 +0300

    Попытка разделить задачи синхронизации и классификации
    Цель задачи синхронизации - понять, что стало с картинками в хранилище.
    До разделения эта задача для новых или измененных фотографиий планировала
    задачу классификации.
    При обновлении модели нейронной сети фотографии не меняются, поэтому в
    старом варианте они не переклассифицировались.
    Это потребовало запускать переклассификацию отдельно.

--- webapp/webapp/pa_tasks/celery.py
+++ webapp/webapp/pa_tasks/celery.py
@@ -1,6 +1,6 @@
 from celery import Celery
-from celery.schedules import crontab
 from celery.utils.log import get_task_logger
+from sqlalchemy import func
 import requests
 import dropbox
 
@@ -17,10 +17,11 @@ logger = get_task_logger(__name__)
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     '''
-    
+
     '''
     sender.add_periodic_task(300.0, bypass_storages.s(), name='run resync every 300 sec')
     sender.add_periodic_task(300.0, update_cnn_version.s(), name='update cnn version')
+    sender.add_periodic_task(300.0, reclassify_photos.s(), name='classify photos')
 
 
 @app.task
@@ -42,15 +43,17 @@ def update_cnn_version():
                 return
 
             current_version = db.session.query(Algorithms) \
-                                        .filter(Algorithms.name==cnn_version['cnn_descr']) \
-                                        .filter(Algorithms.create_date==cnn_version['cnn_date']) \
+                                        .filter(Algorithms.name == cnn_version['cnn_descr']) \
+                                        .filter(Algorithms.create_date == cnn_version['cnn_date']) \
                                         .first()
 
             if not current_version:
-                new_cnn_version = Algorithms(name=cnn_version['cnn_descr'], create_date=cnn_version['cnn_date'])
+                new_cnn_version = Algorithms(name=cnn_version['cnn_descr'],
+                                            create_date=cnn_version['cnn_date'])
                 db.session.add(new_cnn_version)
                 db.session.commit()
-                logger.info('Новая версия нейронной сети: name:"%s" create_date:"%s"' % (cnn_version['cnn_descr'], cnn_version['cnn_date']))
+                logger.info('Новая версия нейронной сети - name:"%s" create_date:"%s"' %
+                            (cnn_version['cnn_descr'], cnn_version['cnn_date']))
 
         except requests.exceptions.RequestException as ex:
             logger.error(ex)
@@ -79,16 +82,20 @@ def sync_dropbox_storage(storage):
         Если картинка есть в базе То:
             Если картинка изменилась То:
                 Обновляем метаинформацию у картинки
-                Ставим картинке признак необходимости провести дополнительные действия, например классификацию
-            Иначе:
-                Ставим картинке признак ОК
+            Ставим картинке признак ОК
         Иначе:
             Создаем картинку в базе
-            Ставим картинке признак необходимости провести дополнительные действия, например классификацию
 
     Шаг3.
     Удаляем информацию о картинках, которые до сих пор инвалидны
     '''
+
+    root_folder = db.session.query(Folders) \
+                            .filter(Folders.storage_user_id == storage.id) \
+                            .filter(Folders.local_path == '/') \
+                            .first()
+    if not root_folder:
+        return
 
     # Step0 - Get ACCESS_TOKEN
     if (storage is None) or (storage.storage_id != 1):
@@ -106,38 +113,25 @@ def sync_dropbox_storage(storage):
     dbx = dropbox.Dropbox(ACCESS_TOKEN)
     result = dbx.files_list_folder("", recursive=True, include_media_info=True)
 
-    root_folder = db.session.query(Folders).filter(Folders.storage_user_id==storage.id).filter(Folders.local_path=='/').first()
-    if root_folder is None:
-        root_folder = Folders(
-                local_path='/',
-                storage_user_id=storage.id)
-        db.session.add(root_folder)
-        db.session.commit()
-        db.session.refresh(root_folder)
-
     def process_file(fmd):
         if not fmd.name.endswith('.jpg'):
             return
-        image_in_db = db.session.query(Photos).filter(Photos.remote_id==fmd.id).first()
+        image_in_db = db.session.query(Photos).filter(Photos.remote_id == fmd.id).first()
         if image_in_db is None:
             logger.info('Картинка "{}" в базе не найдена'.format(fmd.path_display))
 
             new_image = Photos(
                     create_date=fmd.server_modified,
-                    folder_id = root_folder.id,
+                    folder_id=root_folder.id,
                     remote_id=fmd.id,
                     path=fmd.path_lower,
                     revision=fmd.rev,
                     content_hash=fmd.content_hash,
                     name=fmd.name,
                     size=fmd.size,
-                    status=Photos.STATUS_NEED_CLASSIFY)
+                    status=Photos.STATUS_OK)
             db.session.add(new_image)
             db.session.commit()
-            db.session.flush()
-            db.session.refresh(new_image)
-
-            get_class.delay(new_image.id, storage.id)
         else:
             if image_in_db.revision == fmd.rev:
                 logger.info('Картинка "{}" не менялась'.format(image_in_db.path))
@@ -153,30 +147,34 @@ def sync_dropbox_storage(storage):
                 image_in_db.revision = fmd.rev
                 image_in_db.content_hash = fmd.content_hash
                 image_in_db.size = fmd.size
-                image_in_db.status = Photos.STATUS_NEED_CLASSIFY
+                image_in_db.status = Photos.STATUS_OK
                 db.session.add(image_in_db)
                 db.session.commit()
-                db.session.flush()
 
-                get_class.delay(image_in_db.id, storage.id)
+                delete_classes(image_in_db.id)
 
     def process_entries(entries):
         for entry in entries:
              if isinstance(entry, dropbox.files.FileMetadata):
                  process_file(entry)
 
-    ### Main loop
+    # Main loop
     process_entries(result.entries)
 
     while result.has_more:
         result = dbx.files_list_folder_continue(result.cursor)
 
         process_entries(result.entries)
-    ### End of main loop
+    # End of main loop
 
     # Step3
-    for image in Photos.query.filter(Photos.status==Photos.STATUS_NEED_RESYNC):
-        logger.info('Картинка {} не была найдена в Dropbox'.format(image.path))
+    for image in Photos.query.filter(Photos.status == Photos.STATUS_NEED_RESYNC):
+        logger.info('Картинка %s не была найдена в Dropbox и будет удалена.' % image.path)
+        delete_photo(image)
+
+
+def sync_yandex_storage(storage):
+    pass
 
 
 @app.task
@@ -193,23 +191,70 @@ def sync_file_list(id_storage):
         if storage is None:
             return
 
+        # TODO: Можно сделать отдельные таски для каждого типа стораджей
         if storage.storage_id == 1:
             sync_dropbox_storage(storage)
+        if storage.storage_id == 2:
+            sync_yandex_storage(storage)
 
 
 @app.task
-def classify_photos():
+def reclassify_photos():
     '''
     Для каждой неотклассифицированной фотографии запускает отдельную задачу классификации
     '''
-    pass
+    logger.info('Task 3: reclassify_photos')
+
+    app = create_app()
+    with app.app_context():
+        current_cnn_version = get_current_cnn_version()
+        photos = db.session.query(Photos.id.label('id'), StorageUsers.id.label('id_storage')) \
+                       .join(Folders) \
+                       .join(StorageUsers) \
+                       .filter(StorageUsers.storage_id == 1).all()
+        for photo in photos:
+            logger.info('Проверяем картинку с id: %s, id_storage: %s' % (photo.id, photo.id_storage))
+            c_q = db.session.query(func.count(photosclasses.c.alg_id)) \
+                        .select_from(photosclasses) \
+                        .filter(photosclasses.c.photo_id == photo.id) \
+                        .filter(photosclasses.c.alg_id == current_cnn_version) \
+                        .group_by(photosclasses.c.alg_id)
+            cnt = db.session.execute(c_q).scalar()
+            if cnt is None or cnt == 0:
+                logger.info('Отправляем на классификацию картинку с id: %s' % photo.id)
+                get_class.delay(photo.id, photo.id_storage)
+
+
+# TODO: часть этого надо точно где-то в модели делать
+def delete_photo(photo):
+    '''
+    Удаляет фото из базы, предварительно удалив все метки
+    '''
+    d = photosclasses.delete().where(photosclasses.c.photo_id == photo.id)
+    db.session.execute(d)
+    db.session.delete(photo)
+    db.session.commit()
+
+
+# TODO: часть этого надо точно где-то в модели делать
+def delete_classes(id_photo):
+    '''
+    Удаляет у фотографии метки классов, проставленных текущей нейронкой
+    '''
+    current_cnn_version = get_current_cnn_version()
+    d = photosclasses.delete() \
+                     .where(photosclasses.c.photo_id == id_photo) \
+                     .where(photosclasses.c.alg_id == current_cnn_version)
+    db.session.execute(d)
+    db.session.commit()
 
 
 def get_current_cnn_version():
     '''
     Возвращает id текущей версии нейронки
     '''
-    return 0
+    current_cnn = db.session.query(Algorithms).order_by(Algorithms.create_date.desc()).first()
+    return current_cnn.id
 
 
 @app.task(bind=True)
@@ -222,9 +267,8 @@ def get_class(self, id_file, id_storage):
     Шаг3. Полученные классы разложить по таблицам
 
     В случае ошибок, задача перезапускается
-    https://www.distributedpython.com/2018/09/04/error-handling-retry/
     '''
-    logger.info('Task 3: get_class')
+    logger.info('Task 4: get_class')
 
     app = create_app()
     with app.app_context():
@@ -235,7 +279,10 @@ def get_class(self, id_file, id_storage):
         ACCESS_TOKEN = storage.credentials
         dbx = dropbox.Dropbox(ACCESS_TOKEN)
 
-        image_in_db = db.session.query(Photos).filter(Photos.id==id_file).filter(Photos.status==Photos.STATUS_NEED_CLASSIFY).first()
+        image_in_db = db.session.query(Photos) \
+                                .filter(Photos.id == id_file) \
+                                .filter(Photos.status == Photos.STATUS_OK) \
+                                .first()
         if not image_in_db:
             return
 
@@ -255,25 +302,20 @@ def get_class(self, id_file, id_storage):
             result = cnn_response.json()
             if result.get('status', '') != 'OK':
                 logger.error('Проблема работы с cnn сервисом: %s' % result)
-                raise self.retry(exc=ex)
+                raise self.retry()
 
-            prediction = result.get('prediction',{})
+            prediction = result.get('prediction', {})
 
             logger.info('Записываем информацию о картинке: %s' % image_in_db.path)
 
-            # Удаляем все классы, которые были получены текущей CNN
-            current_cnn_version = get_current_cnn_version()
-            d = photosclasses.delete() \
-                        .where(photosclasses.c.photo_id==image_in_db.id) \
-                        .where(photosclasses.c.alg_id==current_cnn_version)
-            db.session.execute(d)
-
             labels = prediction.get('labels', {})
             for label_name, weight in labels.items():
-                class_for_label = db.session.query(Classes).filter(Classes.name==label_name).first()
+                class_for_label = db.session.query(Classes) \
+                                            .filter(Classes.name == label_name) \
+                                            .first()
                 if class_for_label:
                     photo_label = photosclasses.insert().values(
-                            alg_id=current_cnn_version,
+                            alg_id=get_current_cnn_version(),
                             photo_id=image_in_db.id,
                             class_id=class_for_label.id,
                             weight=weight)
@@ -282,9 +324,8 @@ def get_class(self, id_file, id_storage):
             db.session.commit()
 
         except dropbox.exceptions.ApiError as ex:
-            logger.error('Проблемы с выкачиванием картинки id:{}'.format(id_file))
+            logger.error('Проблемы с выкачиванием картинки id: %s' % id_file)
             raise self.retry(exc=ex)
         except requests.exceptions.RequestException as ex:
             logger.error('Проблема работы с cnn сервисом')
             raise self.retry(exc=ex)
-
--- webapp/webapp/pa_tasks/celeryconfig.py
+++ webapp/webapp/pa_tasks/celeryconfig.py
@@ -1,6 +1,8 @@
 task_routes = {
+    'webapp.pa_tasks.celery.update_cnn_version': {'queue': 'celery'},
     'webapp.pa_tasks.celery.bypass_storages': {'queue': 'celery'},
-    'webapp.pa_tasks.celery.sync_file_list': {'queue': 'q2'},
-    'webapp.pa_tasks.celery.get_class': {'queue': 'q3'},
+    'webapp.pa_tasks.celery.sync_file_list': {'queue': 'sync'},
+    'webapp.pa_tasks.celery.reclassify_photos': {'queue' : 'reclassify'},
+    'webapp.pa_tasks.celery.get_class': {'queue': 'classify'},
     }
 
